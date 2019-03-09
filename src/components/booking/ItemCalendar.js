dd742cef961e767ef6b7c50fbc8dde9eff918bd1
Lenka <hubackova.lenka@gmail.com>
2019-03-09 16:56:14 +0100

    calendar to hooks

--- src/components/booking/ItemCalendar.js
+++ src/components/booking/ItemCalendar.js
@@ -1,41 +1,30 @@
 import moment from "moment";
 import Calendar from "react-calendar";
-import React, { Component } from 'react';
+import React, { useEffect } from "react";
 import "./ItemCalendar.scss";
 
-class ItemCalendar extends Component {
-  componentDidMount(){
-    this.props.checkDisableDates(this.props.disabledDates)
-  }
-  componentDidUpdate(){
-    this.props.checkDisableDates(this.props.disabledDates)
-  }
+const ItemCalendar = ({ disabledDates, checkDisableDates, handleDateChange, date, invalid }) => {
+  useEffect(() => checkDisableDates(disabledDates));
 
-  getTileDisabled = ({ date }) => {
-    const reserved = this.props.disabledDates.map(i => {
+  const getTileDisabled = ({ date }) => {
+    const reserved = disabledDates.map(i => {
       return i.map(j => {
-        const isDateReserved =  moment(date).isBetween(j.from, j.to, null, []);
-        return isDateReserved
-      })
-    })
+        const isDateReserved = moment(date).isBetween(j.from, j.to, null, []);
+        return isDateReserved;
+      });
+    });
     return moment(date).add(1, "days") < new Date() || reserved.flat().includes(true);
-    
   };
 
-
-  render() {
-    const {handleDateChange, disabledDates, date, invalid } = this.props
-    return (
-      <Calendar
-        className={invalid ? "invalid" : ""}
-        onChange={date =>
-          handleDateChange(date, disabledDates)}
-        value={date}
-        selectRange={true}
-        tileDisabled={this.getTileDisabled}
-      />
-    );
-  }
-}
+  return (
+    <Calendar
+      className={invalid ? "invalid" : ""}
+      onChange={date => handleDateChange(date, disabledDates)}
+      value={date}
+      selectRange={true}
+      tileDisabled={getTileDisabled}
+    />
+  );
+};
 
 export default ItemCalendar;
