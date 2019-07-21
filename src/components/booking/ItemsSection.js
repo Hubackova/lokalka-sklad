8adbde5c8eea965f30576ddb750bf30ab2670012
Hubackova <lenka.h@atlas.cz>
2019-07-21 22:49:11 +0200

    scrolling

--- src/components/booking/ItemsSection.js
+++ src/components/booking/ItemsSection.js
@@ -1,29 +1,21 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
-const ItemsSection = React.memo(
-  ({ item, openType, list, dateSelected, availableItems, handleOpen }) => {
-    if (list.length === 1) return list; //case when there is just one item of some type
-    const someAvailable = availableItems > 0;
-    const availableClass = someAvailable || !dateSelected;
+const ItemsSection = ({item, openType, list, dateSelected, availableItems, handleOpen}) => {
+  if (list.length === 1) return list //case when there is just one item of some type
+  const someAvailable = availableItems > 0
+  const availableClass = someAvailable || !dateSelected
 
-    return (
-      <>
-        <div
-          id={item.type}
-          className={`item-select main${availableClass ? "" : " notAvailable"}`}
-          onClick={handleOpen}
-        >
-          {item.name}
-          <span className="item-select-number">
-            {dateSelected ? `${availableItems} / ${list.length}` : list.length}
-          </span>
-        </div>
-        {item.type === openType && list}
-      </>
-    );
-  }
-);
+  return (
+    <>
+      <div id={item.type} className={`item-select main${availableClass ? '' : ' notAvailable'}`} onClick={handleOpen}>
+        {item.name}
+        <span className="item-select-number">{dateSelected ? `${availableItems} / ${list.length}` : list.length}</span>
+      </div>
+      <div className="item-section">{item.type === openType && list}</div>
+    </>
+  )
+}
 
 ItemsSection.propTypes = {
   availableItems: PropTypes.number,
@@ -33,6 +25,6 @@ ItemsSection.propTypes = {
   list: PropTypes.node,
   openType: PropTypes.string,
   someAvailable: PropTypes.bool
-};
+}
 
-export default ItemsSection;
+export default ItemsSection
